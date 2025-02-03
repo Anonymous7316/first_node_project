14ec6fb6cf72fe62b2f43f86b49dc6f71d32c619
Dhawal Sarin <dhawal12sarin@gmail.com>
2025-02-03 20:41:19 +0530

    added mongoDb database connection.

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const UserSchema = new mongoose.Schema({
-    name:{type:String, required:true},
+    name:{type:String, required:true, unique: true},
     role:{type:String, require:true}
 });

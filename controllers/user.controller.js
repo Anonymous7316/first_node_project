1494de131ec50ea8bcac54724c5f56bcf5bef53c
Dhawal Sarin <dhawal12sarin@gmail.com>
2025-02-03 23:40:35 +0530

    get user by Id added.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { User } from "../models/user.model.js";
+import { get } from "mongoose";
 
 /**
  * Retrieves all users from the database.
@@ -13,6 +14,27 @@ export const getUser = async(req,res) =>{
     //const userData = await axios('https://jsonplaceholder.typicode.com/users');//Instead define Mongoose Model
     try{
         const userData = await User.find({});
+        res.status(200).send(userData);
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({message:err})
+    }
+}
+
+/**
+ * Retrieves a user by their ID.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.params - The request parameters.
+ * @param {string} req.params.id - The ID of the user to retrieve.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the user data is sent in the response.
+ * @throws {Error} - If an error occurs while retrieving the user data.
+ */
+export const getUserById = async(req,res) =>{
+    try{
+        const userData = await User.findById(req.params.id);
         res.status(200).send(userData);
     }
     catch(err){
@@ -36,8 +58,13 @@ export const addUser = async (req,res)=>{
         res.status(200).send("Data Inserted!");
     }
     catch(err){
-        console.log(err)
-        res.status(400).send({message:err})
+        console.log(err);
+        if(err.code === 11000){
+            res.status(400).send({message:"Duplicate Entry!"})
+        }
+        else{
+            res.status(400).send({message:err})
+        }
     }
 }
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getUser, addUser } from "../controllers/user.controller.js";
+import { getUser, getUserById, addUser } from "../controllers/user.controller.js";
 import { validateUserData } from "../middlewares/user.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', getUser).post('/addUser', validateUserData, addUser);
+userRouter.get('/', getUser).get('/:id', getUserById).post('/addUser', validateUserData, addUser);
 
 export default userRouter;
